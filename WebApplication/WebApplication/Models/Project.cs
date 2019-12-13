09d887fcabf538701375122f8e04cd440133c399
akiratatsuhisa <45509610+akiratatsuhisa@users.noreply.github.com>
2019-12-13 09:42:30 +0700

    Update layout for Management Area, Identity Area, fix layout Projects Controller

--- WebApplication/WebApplication/Models/Project.cs
+++ WebApplication/WebApplication/Models/Project.cs
@@ -9,21 +9,26 @@ namespace WebApplication.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Giảng viên")]
         [Required]
         public string LecturerId { get; set; }
 
         public virtual Lecturer Lecturer { get; set; }
-
+        
+        [Display(Name = "Loại đồ án")]
         public short ProjectTypeId { get; set; }
 
         public virtual ProjectType ProjectType { get; set; }
 
+        [Display(Name = "Tên đồ án")]
         [Required]
         [StringLength(256)]
         public string Title { get; set; }
 
+        [Display(Name = "Mô tả")]
         public string Description { get; set; }
 
+        [Display(Name = "Trạng thái")]
         public byte Status { get; set; }
 
         public virtual ICollection<ProjectMember> ProjectMembers { get; set; }
