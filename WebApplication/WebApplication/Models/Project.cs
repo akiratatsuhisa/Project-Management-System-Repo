28da84736d5b6d8ae548f7f3e7ddf1299bddb159
akiratatsuhisa <45509610+akiratatsuhisa@users.noreply.github.com>
2019-12-14 21:26:18 +0700

    update project status

--- WebApplication/WebApplication/Models/Project.cs
+++ WebApplication/WebApplication/Models/Project.cs
@@ -38,5 +38,24 @@ namespace WebApplication.Models
 
         public virtual ICollection<ProjectSchedule> ProjectSchedules { get; set; }
 
+        public ProjectStatusViewModel StatusViewModel
+        {
+            get => Status switch
+            {
+                0 => new ProjectStatusViewModel { Name = "Đang tiến hành", TextClassName = "text-primary" },
+
+                2 => new ProjectStatusViewModel { Name = "Không được tiếp tục", TextClassName = "text-warning" },
+
+                4 => new ProjectStatusViewModel { Name = "Đợi chấm", TextClassName = "text-info" },
+
+                6 => new ProjectStatusViewModel { Name = "Đạt", TextClassName = "text-success" },
+
+                8 => new ProjectStatusViewModel { Name = "Rớt", TextClassName = "text-danger" },
+
+                10 => new ProjectStatusViewModel { Name = "Bị hủy", TextClassName = "text-danger" },
+
+                _ => new ProjectStatusViewModel { Name = "Không có trạng thái" }
+            };
+        }
     }
 }
---
+++ WebApplication/WebApplication/Models/ProjectStatusViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication.Models
+{
+    public class ProjectStatusViewModel
+    {
+        public string Name { get; set; }
+
+        public string TextClassName { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
